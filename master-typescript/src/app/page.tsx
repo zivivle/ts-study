a5fa576480eac493f0e7f223bd67acd8dad63dd3
zivivle <zivivle@gmail.com>
2024-01-26 14:51:45 +0900

    design: 타입스크립트 퀴즈 페이지 메인 퍼블리싱

--- master-typescript/src/app/page.tsx
+++ master-typescript/src/app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import mainImage from "/public/images/main_image.png";
+import mainButton from "/public/images/main_button.png";
 
 export default function Home() {
   return (
-    <div>
-      <div>hello typescript</div>
+    <div className="flex flex-col justify-center items-center h-[100dvh]">
+      <Image src={mainImage} alt="mainImage" width={600} height={300} />
+      <div>
+        <button className="border-[2px] border-black rounded-2xl p-8 mt-10 hover:bg-amber-100">
+          <Image
+            src={mainButton}
+            alt="mainButtonImage"
+            width={400}
+            height={300}
+          />
+        </button>
+      </div>
     </div>
   );
 }
